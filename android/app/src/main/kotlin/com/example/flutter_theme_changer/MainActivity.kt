4dd77c92dfda824316f8b433501f83cd7fcd1298
Atif <atif@Apples-MacBook-Air.local>
2025-05-15 15:22:56 +0500

    Add updated Flutter theme changer app

---
+++ android/app/src/main/kotlin/com/example/flutter_theme_changer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_theme_changer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp(
+      themeMode: themeProvider.themeMode,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
